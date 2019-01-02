10af47a0bab37c42565bc04876ba30aebb2cef36
Ethan Young <Eyoung8@wisc.edu>
2019-01-02 11:21:31 -0600

    Delete Secrets.h
    Should have been in gitignore

--- src/MicrocontrollerCode/Secrets.h
+++
@@ -1,7 +0,0 @@
-/* Define secrets that should not be on github */
-#define END_POINT_SECRET "https://flowmetrichandler.azurewebsites.net/api/HttpTrigger1?code=8dsSULNVuQOuPEfa/kKhUFa1f3mwkboWvSdutc2iiZm8a5IL9hwWKw=="
-#define HOST_SECRET "flowmetrichandler.azurewebsites.net"
-#define FINGERPRINT_SECRET "3A:B0:B1:C2:7F:74:6F:D9:0C:34:F0:D6:A9:60:CF:73:A4:22:9D:E8"
-
-#define OTA_USERNAME_SECRET "flow_meter_username"
-#define OTA_PASSWORD_SECRET "flow_meter_password" 
