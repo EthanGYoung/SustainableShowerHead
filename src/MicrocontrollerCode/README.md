5e62bd006c5bdf2589b3de50f86776745e8ed1d2
Ethan Young <eyoung8@wisc.edu>
2019-01-02 11:31:25 -0600

    Added an example secrets file for the micorcontroller code

---
+++ src/MicrocontrollerCode/README.md
@@ -0,0 +1,1 @@
+When uploading code to the microcontroller, enter all of the required secrets into Secrets_example.h and then change the name to Secrets.h. The code will compile now.
---
+++ src/MicrocontrollerCode/Secrets_Example.h
@@ -0,0 +1,9 @@
+/* Define secrets that should not be on github */
+#define END_POINT_SECRET "" // The endpoint of your website or lambda function
+#define HOST_SECRET "test_handler.azurewebsites.net" // Before the /api of your endpoint
+
+// Find this online by typing host name into https://www.grc.com/fingerprints.htm
+#define FINGERPRINT_SECRET ""
+
+#define OTA_USERNAME_SECRET "OTA_username"
+#define OTA_PASSWORD_SECRET "OTA_password"
